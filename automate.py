5c59043a0464e4f8aed10a86316872c1db94ba72
THIBAULT <thibault.colchen@yahoo.fr>
2022-02-03 14:24:14 +0100

    Update automate.py

--- automate.py
+++ automate.py
@@ -1,26 +1,41 @@
 from readfile import *
 import shutil
 import time
-#Dictionnary containing key : the variable name, value : the line number in the .dck file -1 (in the dck file line number start at 1, in python list number start at 0 )
+
+""""
+Dictionnary containing:
+    key :the variable name,
+    value : the line number in the .dck file -1 (in the dck file line number start at 1, in python list number start at 0 )
+"""
 building_line = {"Building loss coefficient": 134,"Building capacitance":135,"Building surface area":138,"Building volume":139}
 window_line = {"Window South":500,"Window East":501,"Window West":502,"Window North":503}
 power_line = {"Heating power":225,"Cooling power":226}
-
 external_line= {"Ventilation losses":156,"Energy gain from people":156}
 
-#liste of dictionnaries
-building_value, window_value, power_value, external_value = create_dictionnary(nb_simulation)
+deltaTlines = {"ONTh":482,"OFFTh":483,"ONTc":484,"OFFTc":485}
 
+
+#list of dictionnaries
+building_value, window_value, power_value, external_value,dT = create_dictionnary(nb_simulation)
+
+"""
+Defining functions to create files 
+"""
+
+#Create files to executes in TRNSYS by copying an existing one saved in a specific folder
 def create_file(nb):
 	for n in range(nb):
 		f_string = "C:\\TRNSYS18\\Automatisation\\deckfiles\\simu" + str(n) + ".dck"
 		shutil.copy("C:\\TRNSYS18\\MyProjects\\Automatisation\\Run1.dck",f_string)
-
+#Create files to save results at the end of simulations
 def create_results(num):
 	r_string = "C:\\TRNSYS18\\Automatisation\\Results\\ResultSimu" + str(num) + ".plt"
 	shutil.copy("C:\\TRNSYS18\\MyProjects\\trnsysfiles\\testauto.plt",r_string)
 
-
+"""
+Changing the data of the executable files
+"""
+#Replace the initial value of copied file by wanted value of simulations' parameter
 def modif_deck(filename,simu): 
 	with open(filename,"r") as data :
 		deck_file = data.readlines()
@@ -36,18 +51,22 @@ def modif_deck(filename,simu):
 	for key in external_value[simu].keys():
 		deck_file[external_line[key]]= replace_external(deck_file[external_line[key]],key,simu)
 
+	for key in dT[simu].keys():
+		deck_file[deltaTlines[key]] = replace_deltaT(deck_file[deltaTlines[key]],key,simu)
 
 	file = open(filename,"w")
 	str_deck_file = "".join(deck_file)
 	file.write(str_deck_file)
 	file.close()
 
+#Write a value to replace values of : ventilation losses and internal gain
 def replace_external(fileline,key,simu):
 	value_tab = fileline.split(" ")
 	value_tab[2] = external_value[simu]["Ventilation losses"]
 	value_tab[8] = external_value[simu]["Energy gain from people"]
 	return " ".join(value_tab)
 
+#Write a value to replace internal parameters of the building 
 def replace_building_params(fileline,key,simu):
 	list_line = list(fileline)
 	ind = 0
@@ -59,6 +78,8 @@ def replace_building_params(fileline,key,simu):
 	left_part = building_value[simu][key]
 	new_line = "".join(left_part) +"		"+ "".join(right_part)
 	return new_line
+
+#Write a value to replace value of the power of heating device
 def replace_power(fileline,key,simu):
 	list_line = list(fileline)
 	ind1= 0
@@ -76,6 +97,7 @@ def replace_power(fileline,key,simu):
 	return new_line
 
 
+#Write a value to replace the window sizes
 def replace_window(fileline,key,simu):
 	list_line = list(fileline)
 	ind = 0
@@ -87,4 +109,22 @@ def replace_window(fileline,key,simu):
 	right_part = window_value[simu][key]
 	new_line = "".join(left_part) +" = "+"".join(right_part)+"\n"
 	
+	return new_line
+
+#Write a value to replace the sensitivity for turning on the heating power
+def replace_deltaT(fileline,key,simu):
+	list_line = list(fileline)
+	ind = 0
+
+	for l in range(len(list_line)):
+		if list_line[l]=="=":
+			ind = l
+			break
+
+	left_part = list_line[:ind]
+
+	right_part = dT[simu][key]
+	#print(deltaT[key])
+	new_line = "".join(left_part) +"= "+"".join(right_part)+"\n"
+
 	return new_line
